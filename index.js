1ed4387843dd31496255ea0c1b84273cae154861
ui-kenjie <132860983+ui-kenjie@users.noreply.github.com>
2024-07-11 15:48:34 +0800

    adding pagination

--- index.js
+++ index.js
@@ -11,27 +11,60 @@ const apiCall = async () => {
     const apiCallObj = await apiCallPromise.json();
     return apiCallObj;
 };
-const tableContent = () => {
+
+
+const tableContent = (page_number, list_number) => {
     
-    let x = 20;
-    let i = 0;
+    let startTo_page = page_number;
+    let numberOf_item_perPage = 9;
 
     apiCall().then( objectText => {
         let tableSample = document.querySelector('#tableSample tbody');
+        let tablePagination = document.querySelector('.tableWrapper') 
         
         const array2 = objectText;
-        array2.forEach(element => {
+        let array3 = [];
+        for(c=startTo_page; c < startTo_page+numberOf_item_perPage; c++) {
+            if(c <= array2.length) {
+                array3.push(array2[c-1])
+            } 
+        }
 
-                if(i < x ) {
-                    tableSample.innerHTML += `<tr>  <td>${element.userId}</td> <td>${element.id}</td>  <td>${element.title}</td>  <td>${element.completed}</td>  </tr>`
-                }
-                
-                i = i +1;
+        array3.forEach(element => {
+                tableSample.innerHTML += `<tr>  <td>${element.userId}</td> <td>${element.id}</td>  <td>${element.title}</td>  <td>${element.completed}</td>  </tr>`;
         });
 
+        let total_pages = Math.ceil(array2.length/numberOf_item_perPage)
+        if (total_pages > 1) {
+                        tablePagination.innerHTML += '<div class="table-pagination"><ul class="pagination float-end mt-3"> </ul></div>'
+                        let tablePaginationlist = document.querySelector('.pagination') 
+                        for(i=1; i <= total_pages; i++) {
+                        tablePaginationlist.innerHTML += `<li pagenum="${i}">${i}</li>`
+         }
+         btnpagination_eventListener()
+        }
+        
+        
     })
 
 }
 
-console.log(apiCall())
-tableContent()
+const btnpagination_eventListener = () => {
+    let btn_pagination123 = document.querySelectorAll('.pagination > li');
+
+    btn_pagination123.forEach(btn_pagenum => {
+        btn_pagenum.addEventListener('click', (e) => {
+            let mainTable = document.querySelector('#tableSample tbody');
+            let tablePaginationlist = document.querySelector('.pagination');
+            tablePaginationlist.innerHTML = '';
+            mainTable.innerHTML = '';
+            let pagenum = (parseInt(e.target.getAttribute('pagenum')-1) *10) +1
+            tableContent(pagenum)
+        })
+    })
+}
+
+tableContent(21)
+
+
+
